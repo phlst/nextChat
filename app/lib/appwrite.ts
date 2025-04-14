2fdd154215874a1f6efba02c2e1253d085b29bb1
phlst <hajdu2024@gmail.com>
2025-04-14 20:39:21 +0200

    Working login and register

--- app/lib/appwrite.ts
+++ app/lib/appwrite.ts
@@ -66,3 +66,21 @@ export async function signUpWithEmail(formData: FormData) {
   });
   redirect('/messenger');
 }
+
+export async function logInWithEmail(formData: FormData) {
+  const email = formData.get('email') as string;
+  const password = formData.get('password') as string;
+
+  const { account } = await createAdminClient();
+  const session = await account.createEmailPasswordSession(email, password);
+
+  const cookieStore = await cookies();
+
+  cookieStore.set('session', session.secret, {
+    path: '/',
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: true,
+  });
+  redirect('/messenger');
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,46 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FormEvent, useRef } from 'react';
+import { logInWithEmail } from './lib/appwrite';
 
 export default function Home() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!formRef.current) return;
+
+    const formData = new FormData(formRef.current);
+
+    try {
+      await logInWithEmail(formData);
+    } catch (error) {
+      console.error('Error signing up:', error);
+      alert(error instanceof Error ? error.message : 'Failed to sign up');
+    }
+  };
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <div className='bg-custom-gray/20 flex h-full w-full flex-col items-center p-4 text-white shadow-lg lg:h-[60%] lg:w-[50%] lg:rounded-tl-lg lg:rounded-tr-4xl lg:rounded-br-lg lg:rounded-bl-4xl lg:border-1 lg:border-zinc-300/30'>
-        <form className='flex h-[50%] w-[60%] flex-col items-center justify-between text-white'>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='flex h-[50%] w-[60%] flex-col items-center justify-between text-white'
+        >
           <h1 className='mb-5 text-5xl font-bold'>Log in</h1>
           <input
             className='bg-custom-black mt-2 w-full rounded-lg border-1 border-zinc-50/10 p-4 transition-colors outline-none focus:border-zinc-50/30'
             placeholder='example@gmail.com'
             type='email'
+            name='email'
           />
           <input
             className='bg-custom-black w-full rounded-lg border-1 border-zinc-50/10 p-4 transition-colors outline-none focus:border-zinc-50/30'
             type='password'
             placeholder='Your password'
+            name='password'
           />
           <div className='mt-1 flex w-full justify-end'>
             <a href='#' className='text-custom-green text-sm hover:underline'>
